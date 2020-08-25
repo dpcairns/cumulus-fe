d3c50994445b4ecc440e682fc5b7a7b527a68101
warrioryoko <miangpellegri@gmail.com>
2020-08-25 15:07:39 -0700

    Created working dropdown with country name displaying, country code as value selected.

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import CountryData from './CountryData.js';
 
 export default class Dropdown extends Component {
 
     state = {
-        country: 'US'
+        country: 'US',
+        CountryData: CountryData,
     }
 
 
@@ -11,14 +13,23 @@ export default class Dropdown extends Component {
         e.preventDefault();
 
         this.setState({ country: e.target.value });
+        //console.log(this.state.country);
     }
 
     render() {
+        
         return (
             <div>
                 <form>
                     <label>
                         <select value={this.state.value} onChange={this.handleDropdownChange}>
+                        {/* {
+                                this.state.CountryData.map((country) => <option value={country.countries}>{country.countries}</option>)
+                            } */}
+                            {
+            //const countryNames = Object.values(CountryData);
+            (Object.values(this.state.CountryData)).map((country) => <option value={country.iso2}>{country.name}</option>)
+        }
                             <option value="US"></option>
                         </select>
                     </label>
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Dropdown from './Dropdown.js';
 
 export default class SearchPage extends Component {
     render() {
         return (
             <div>
-
+                <Dropdown></Dropdown>
             </div>
         )
     }

6c2e18830b83729281da0d6c7339e569828c4317
Heather Peterson <hpeterson462@gmail.com>
2020-08-26 19:17:32 -0700

    about us page outline

--- src/About.js
+++ src/About.js
@@ -4,7 +4,15 @@ export default class About extends Component {
     render() {
         return (
             <div>
-                About Us
+                <h1>About Us</h1>
+                <h3>Annalise Murphy</h3>
+                <p>Annalise is a fullstack software developer and playa-sandstorm-slayer. When not programming rad apps, they enjoy being outdoors and ariel arts.</p>
+                <h3>Heather Peterson</h3>
+                <p>Heather is a former healthcare worker turned fullstack software developer. When not staring at a computer screen, she enjoys hiking and rock climbing.</p>
+                <h3>Katie Perry</h3>
+                <p>Katie has enjoyed teaching in the past and is now a fullstack software developer. When not schooling her keyboard to code, she enjoys drinking coffee and hiking.</p>
+                <h3>Ron Yonker</h3>
+                <p>Ron is a fullstack software developer and IT wrangler. When not taming code, he enjoys .</p>
             </div>
         )
     }

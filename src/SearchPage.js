d49f5f137260af8d2dc2c52daa3e9119136d9210
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-08-26 19:41:40 -0700

    change temp c to f

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -112,9 +112,9 @@ export default class SearchPage extends Component {
                 <div className="results-div">
                     {
                         <section>
-                            <p>country_code: {this.state.country_code}</p>
-                            <p>location: {this.state.location}</p>
-                            <p>temp: {this.state.temp}</p>
+                            <p>Country: {this.state.country_code}</p>
+                            <p>City: {this.state.location}</p>
+                            <p>Temperature: {this.state.temp ? Math.ceil(((this.state.temp)*1.8) + 32)+'°' : ''} </p>
                         </section>
                     }
                 </div>

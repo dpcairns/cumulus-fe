6e98c957eef7383c8f5e5068fc022bf6030f65af
Heather Peterson <hpeterson462@gmail.com>
2020-08-27 15:41:21 -0700

    about us text

--- src/About.js
+++ src/About.js
@@ -5,14 +5,18 @@ export default class About extends Component {
         return (
             <div>
                 <h1>About Us</h1>
-                <h3>Annalise Murphy</h3>
-                <p>Annalise is a fullstack software developer and playa-sandstorm-slayer. When not programming rad apps, they enjoy being outdoors and aerial arts.</p>
-                <h3>Heather Peterson</h3>
-                <p>Heather is a former healthcare worker turned fullstack software developer. When not staring at a computer screen, she enjoys hiking and rock climbing.</p>
-                <h3>Katie Perry</h3>
-                <p>Katie has enjoyed teaching in the past and is now a fullstack software developer. When not schooling her keyboard to code, she enjoys drinking coffee and hiking.</p>
-                <h3>Ron Yonker</h3>
-                <p>Ron is a fullstack software developer and IT wrangler. When not taming code, he enjoys .</p>
+                <img src={/sun.png} alt="sun" / >
+                    <h3>Annalise Murphy</h3>
+                    <p>Annalise is a fullstack software developer and playa-sandstorm-slayer. When not programming rad apps, they enjoy being outdoors and aerial arts.</p>
+                    <img src={/snow.png} alt="snow" / >
+                        <h3>Heather Peterson</h3>
+                        <p>Heather is a former healthcare worker turned fullstack software developer. When not staring at a computer screen, she enjoys hiking and rock climbing.</p>
+                        <img src={/cloud.png} alt="cloud" / >
+                            <h3>Katie Perry</h3>
+                            <p>Katie has enjoyed teaching in the past and is now a fullstack software developer. When not schooling her keyboard to code, she enjoys drinking coffee and hiking.</p>
+                            <img src={/wind.png} alt="wind" / >
+                                <h3>Ron Yonker</h3>
+                                <p>Ron is a fullstack software developer and IT wrangler. When not taming code, he enjoys sleeping and eating.</p>
             </div>
         )
     }

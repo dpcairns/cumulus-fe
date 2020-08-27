e99985f73f9ddd1a2dc1420b9b26c018acd4d28b
Heather Peterson <hpeterson462@gmail.com>
2020-08-27 09:20:02 -0700

    about me page

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ export default class About extends Component {
             <div>
                 <h1>About Us</h1>
                 <h3>Annalise Murphy</h3>
-                <p>Annalise is a fullstack software developer and playa-sandstorm-slayer. When not programming rad apps, they enjoy being outdoors and ariel arts.</p>
+                <p>Annalise is a fullstack software developer and playa-sandstorm-slayer. When not programming rad apps, they enjoy being outdoors and aerial arts.</p>
                 <h3>Heather Peterson</h3>
                 <p>Heather is a former healthcare worker turned fullstack software developer. When not staring at a computer screen, she enjoys hiking and rock climbing.</p>
                 <h3>Katie Perry</h3>
